7ec335a5f2118bb76ebc4cffdea35ffa15d79842
tschasu <tpschafe@asu.edu>
2024-04-24 03:56:44 -0700

    Fixed the button and errors
    Fixed the button's name to fit the disco prompt more, also removed
    an error where window was accessed in the disco methods after being detroyed,
    now the error is caught

--- demonstration.py
+++ demonstration.py
@@ -6,7 +6,7 @@ mainWindow = Tk()
 class ColorChanger:
     def __init__(self, window):
         self.window = window
-        self.button = Button(window, text="Change Color", command=self.changeColor)
+        self.button = Button(window, text="Acivate Disco Ball", command=self.changeColor)
         self.button.pack()
         self.canvas = Canvas(window)
         self.canvas.pack()
@@ -14,17 +14,19 @@ class ColorChanger:
     def changeColor(self):
         (centerX, centerY) = (int(self.canvas['width']) / 2, int(self.canvas['height']) / 2)
         radius = 100
-        
 
-        while (True):
-            # self.window.configure(background=self.generateRandomColor())
-            self.canvas.delete("all")
-            self.canvas.create_oval(centerX - radius, centerY - radius, centerX + radius, centerY + radius, fill=self.generateRandomColor())
-            for i in range(20, 120, 20):
-                self.canvas.create_oval(centerX - (radius - i), centerY - radius, centerX + (radius - i), centerY + radius)
-                self.canvas.create_oval(centerX - radius, centerY - (radius - i), centerX + radius, centerY + (radius - i))
-            self.window.update()
-            time.sleep(0.5)
+        try:
+            while (True):
+                # self.window.configure(background=self.generateRandomColor())
+                self.canvas.delete("all")
+                self.canvas.create_oval(centerX - radius, centerY - radius, centerX + radius, centerY + radius, fill=self.generateRandomColor())
+                for i in range(20, 120, 20):
+                    self.canvas.create_oval(centerX - (radius - i), centerY - radius, centerX + (radius - i), centerY + radius)
+                    self.canvas.create_oval(centerX - radius, centerY - (radius - i), centerX + radius, centerY + (radius - i))
+                self.window.update()
+                time.sleep(0.5)
+        except:
+            print("Complete")
 
     def obtainCurrentColor(self):
         return self.window["background"]
