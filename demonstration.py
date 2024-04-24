8b797771b951d9d8ec5cf463e6fab9e0b69f4e21
tschasu <tpschafe@asu.edu>
2024-04-24 03:40:22 -0700

    Placed a canvas with a centered circle in the window

--- demonstration.py
+++ demonstration.py
@@ -8,14 +8,18 @@ class ColorChanger:
         self.window = window
         self.button = Button(window, text="Change Color", command=self.changeColor)
         self.button.pack()
+        self.canvas = Canvas(window)
+        self.canvas.pack()
 
     def changeColor(self):
+        (centerX, centerY) = (int(self.canvas['width']) / 2, int(self.canvas['height']) / 2)
+        radius = 100
+        self.canvas.create_oval(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+
         while (True):
             self.window.configure(background=self.generateRandomColor())
             self.window.update()
             time.sleep(0.5)
-
-        
 
     def obtainCurrentColor(self):
         return self.window["background"]
