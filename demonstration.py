1d816b0dcbb5b2384a045debdd1ed925993e2f49
tschasu <tpschafe@asu.edu>
2024-04-24 02:35:30 -0700

    Added a class to allow for changing colors
    Added a class that is required in order to have colors dynamically change.

--- demonstration.py
+++ demonstration.py
@@ -1,13 +1,19 @@
 from tkinter import *
 mainWindow = Tk()
 
-def changeColor():
-    print("Does Not Do Anything Yet")
+class ColorChanger:
+    def __init__(self, window):
+        self.window = window
+        self.button = Button(window, text="Change Color", command=self.changeColor)
+        self.button.pack()
+
+    def changeColor(mainWindow):
+        print("Does not do anything yet")
+    
 
 mainWindow.title('Demonstration')
 
-button = Button(mainWindow, text="Change Color", command=changeColor)
-button.pack()
+ColorChanger(mainWindow)
 
 quitButton = Button(mainWindow, text='Quit Demonstration', command=mainWindow.destroy)
 quitButton.pack()
