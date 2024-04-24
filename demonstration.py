77db633fb022443d1ee5a1f87a677bc3ba7b263d
tschasu <tpschafe@asu.edu>
2024-04-24 02:15:49 -0700

    Begin Tinkering With GUI
    I did some initial GUI tinkering.
    I plan to do some branching and merging as I go forwards

--- demonstration.py
+++ demonstration.py
@@ -1,1 +1,9 @@
+import tkinter
+mainWindow = tkinter.Tk()
+
+mainWindow.title('Demonstration')
+button = tkinter.Button(mainWindow, text='Quit Demonstration', command=mainWindow.destroy)
+button.pack()
+mainWindow.mainloop()
+
 print("TODO: DEMONSTRATE")
